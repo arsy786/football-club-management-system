8e5950666842a773e222b626bf83775e62cf58ed
arsalaan.naeem <arsalaannaeem@hotmail.com>
2022-09-05 17:15:12 +0100

    application.properties: - added h2 DB config
    Player.java:
    - removed incorrect import

--- src/main/java/dev/arsalaan/footballclubmanagementsystem/model/Player.java
+++ src/main/java/dev/arsalaan/footballclubmanagementsystem/model/Player.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.graalvm.compiler.core.match.MatchableNode;
 
 import javax.persistence.*;
 
