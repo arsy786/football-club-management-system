0bacc580a8630c4862c6c9e885ff4906606f7c4b
arsalaan.naeem <arsalaannaeem@hotmail.com>
2022-09-14 22:02:52 +0100

    TeamDTO.java: - Added JPA Validation Annotations

--- src/main/java/dev/arsalaan/footballclubmanagementsystem/dto/TeamDTO.java
+++ src/main/java/dev/arsalaan/footballclubmanagementsystem/dto/TeamDTO.java
@@ -2,10 +2,19 @@ package dev.arsalaan.footballclubmanagementsystem.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class TeamDTO {
+
     private Long teamId;
+
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
+
+    @NotEmpty(message = "City should not be null or empty")
     private String city;
+
+    @NotEmpty(message = "Manager should not be null or empty")
     private String manager;
 }
