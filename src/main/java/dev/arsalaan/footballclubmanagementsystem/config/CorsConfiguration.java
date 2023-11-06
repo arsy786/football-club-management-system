f84f707c08ac7b257e60c77a404aa0223230f7c0
arsalaan.naeem <arsalaannaeem@hotmail.com>
2023-11-06 23:50:11 +0000

    cors swagger-ui deployed app

--- src/main/java/dev/arsalaan/footballclubmanagementsystem/config/CorsConfiguration.java
+++ src/main/java/dev/arsalaan/footballclubmanagementsystem/config/CorsConfiguration.java
@@ -12,7 +12,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("https://football-club-management-system-production.up.railway.app/swagger-ui/*")
+        .allowedOrigins("*")
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowedHeaders("*");
   }
